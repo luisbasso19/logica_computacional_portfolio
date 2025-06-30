8725b019db1efb589fe4e7f6451100d822f60d32
Luis Antonio Basso <luisbasso19@gmail.com>
2025-06-30 18:37:28 -0300

    Pequenas modificacoes no codigo, e inclusao de comentarios

--- action/armazenar_dados.py
+++ action/armazenar_dados.py
@@ -3,10 +3,10 @@ from openpyxl import Workbook, load_workbook
 
 
 def armazena_dados(temperatura, umidade, data, erro=None):
-    arquivo_planilha = "capturas.xlsx"
-    pasta_armazenamento = "dados_capturados/"
-    caminho = pasta_armazenamento + arquivo_planilha
-    if not os.path.exists(caminho):
+    arquivo_planilha = "capturas.xlsx" #define uma variavel com o nome do arquvivo que armazena as capturas
+    pasta_armazenamento = "dados_capturados/" #define uma pasta para armazenar o arquivo com as capturas
+    caminho = pasta_armazenamento + arquivo_planilha #concatena as variaveis para formar o caminho completo do nome do arquivo
+    if not os.path.exists(caminho): #verifica se o arquivo nao existe, criando o arquvio e gravando os cabecalhos
         #cria um novo workbook e coloca os cabecalhos
         wb = Workbook()
         ws = wb.active
@@ -16,13 +16,13 @@ def armazena_dados(temperatura, umidade, data, erro=None):
         wb.save(caminho)
 
     if erro:
-        pass
+        return "Favor verificar o coletor de dados!" #quando existir um erro, pede para o usuario verificar o coletor de dados.
     else:
-        wb = load_workbook(caminho)
-        ws = wb.active
+        wb = load_workbook(caminho) #carrega a planilha ja criada
+        ws = wb.active #ativa a planilha
 
-        ws.append((data, temperatura, umidade))
+        ws.append((data, temperatura, umidade)) #adiciona os dados capturados
 
-        wb.save(caminho)
+        wb.save(caminho) #salvo os dados no arquivo de planilha
 
-        return "Dados salvos com sucesso!"
+        return "Dados salvos com sucesso!" #retorna uma mensagem de que os dados foram salvos com sucesso.
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from action.buscar_dados import captura_dados
 from action.armazenar_dados import armazena_dados
 
 def gravar_dados():
-    resultado = captura_dados()
-    if resultado["erro"]:
-        mensagem = armazena_dados(None, None, None, resultado["erro"])        
+    resultado = captura_dados() #chama a funcao que realiza a captura dos dados, e armazena os dados retornados em um dicionario
+    if resultado["erro"]:#verifica se existe algum valor valido na chave erro
+        mensagem = armazena_dados(None, None, None, resultado["erro"]) #chama a variavel que armazena os dados e grava o valor retornado em uma variavel       
+        label_resultado["text"] = mensagem #atualiza o label criado, para que mostre a mensagem de retorno da funcao que armazena os dados na planilha
     else:
-        mensagem = armazena_dados(resultado["temperatura"], resultado["umidade"],resultado["data_hora"])
+        mensagem = armazena_dados(resultado["temperatura"], resultado["umidade"],resultado["data_hora"])#chama a variavel que armazena os dados e grava o valor retornado em uma variavel
 
-    label_resultado["text"] = mensagem
+        label_resultado["text"] = f"{mensagem} | {resultado['data_hora']}" #atualiza o label criado, para que mostre a mensagem de retorno da funcao que armazena os dados na planilha
 
 
 
@@ -22,9 +23,10 @@ texto = tk.Label(janela, text="Atualizar dados na planilha:")
 texto.pack(pady=10) #TEXTO ADICIONADO A JANELA COM ESPACAMENTO VERTICAL
 
 #CRIANDO UM BOTAO
-botao = tk.Button(janela, text="Buscar Dados", command=gravar_dados)
+botao = tk.Button(janela, text="Buscar Dados", command=gravar_dados) #cria o botão e coloca a a acao a ser realizada pelo botao, a chamada da funcao gravar_dados
 botao.pack() #BOTAO ADICIONADO A JANELA COM ESPACEMENTO VERTICAL
 
+#CRIA UMA AREA DE TEXTO PARA MOSTRAR SE OS DADOS FORAM GRAVADOS CORRETAMENTE.
 label_resultado = tk.Label(janela, text="")
 label_resultado.pack()
 
